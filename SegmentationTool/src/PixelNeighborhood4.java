2d8976f5441dafb72dc0e5646b25951f92c17d98
Aleksandar Cirkovic <cirko@web.de>
2014-07-13 13:21:39 +0200

    Change-Id: I16b11c54d0a46f840cb4338432d5b640a4721d79

--- SegmentationTool/src/PixelNeighborhood4.java
+++ SegmentationTool/src/PixelNeighborhood4.java
@@ -28,19 +28,47 @@ public class PixelNeighborhood4 implements PixelNeighborhood {
 	 * @see PixelNeighborhood#getNeighbor(PixelInfo, int, int, int)
 	 */
 	@Override
-	public PixelInfo[] getNeighbor(PixelInfo pixel, int i, int width, int height) {
-		PixelInfo[] neighbors;
-		//calculate the X and Y coordinates from the given index
-		int x = i%width;
-		int y = (i-x)/width;
-		int index=0;
-		for (int ii=-1;ii<=1;ii=ii+2) {
-			for (int jj=-1; jj<=1; jj=jj+2) {
-				neighbors[index] = jj*width+ii;
-				index++;
-			}
+	public PixelInfo getNeighbor(PixelInfo pixel, int i, int width, int height) {
+		PixelInfo neighbor = new PixelInfo(0,0,0);
+		//1 cycle = 8 pixels around the given pixel; i'th neighbor = number of
+		//cycles (n) + rest
+		int rest = i%8;
+		int n=(i/8)+1;
+		int x = pixel.x;
+		int y = pixel.y;
+		switch (rest) {
+		case 1:
+			x=x+n;
+			break;
+		case 2:
+			y=y+n;
+			break;
+		case 3:
+			x=x-n;
+			break;
+		case 4:
+			y=y-n;
+			break;
+		case 5:
+			x=x+n;
+			y=y-n;
+			break;
+		case 6:
+			x=x+n;
+			y=y+n;
+			break;
+		case 7:
+			x=x-n;
+			y=y+n;
+			break;
+		case 8:
+			x=x-n;
+			y=y-n;
+			break;
 		}
-		return neighbors;
+		//now we can calculate the new neighbor-pixel
+		neighbor=PixelInfo.createFromXY(x,y,width,height);
+		return neighbor;
 	}
 
 }
