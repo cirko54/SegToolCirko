c7b388aa7f0e9e99c261b949be925d4b82b643ee
Aleksandar Cirkovic <cirko@web.de>
2014-08-19 22:13:34 +0200

    das wars

--- SegmentationTool/src/SegmentationToolDialog.java
+++ SegmentationTool/src/SegmentationToolDialog.java
@@ -387,7 +387,6 @@ public class SegmentationToolDialog extends JDialog implements ChangeListener,
 		} else {
 			// no modifier key: replace model's selection by this one
 			model.replaceSelection(tmp_select);
-			System.out.println("Ätsch, hab schon wieder ersetzt.");
 		}
 
 		updateModel();
